f15edac779a1302ca02681a0ce17fed86bd6ed55
Xiiiii <1983136940@qq.com>
2023-05-26 11:22:41 +0800

    22

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ var images = document.querySelectorAll('.image');
 		        var moveX = (window.innerWidth - image.offsetWidth * 1.2) / 2 - image.offsetLeft;
 		        var moveY = (window.innerHeight - image.offsetHeight * 1.2) / 2 - image.offsetTop;
 		        image.style.transform += ' translate(' + moveX + 'px, ' + moveY + 'px)';
-		        image.style.zIndex = 999; /*  移动时将z-index值设置为最高  */
+		        image.style.zIndex = 999; 
 		    });
 		    images[i].addEventListener('mouseout', function(e) {
 		        e.target.style.transform = 'none';
-		        e.target.style.zIndex = 1; /*  鼠标移开后恢复z-index值  */
+		        e.target.style.zIndex = 1; 
 		    });
     
 		}
