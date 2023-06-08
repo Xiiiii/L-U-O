fc064715feaeb545511dc09ab14250d0e0b99e01
Xiiiii <1983136940@qq.com>
2023-06-08 15:42:04 +0800

    biojlk

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-function random(){
-  return Math.floor(Math.random() * 60 - 30)
-}
 var images = document.querySelectorAll('.image');
-console.log(images)
 
-		for (var i = 0; i < images.length; i++) {
-        images[i].style.top = random() + "px";
-        images[i].style.left = random() + "px";
-		    images[i].addEventListener('mouseover', function(e) {
-		        var image = e.target;
-		        image.style.transform = 'scale(1.5)';
-		        var moveX = (window.innerWidth - image.offsetWidth * 1.2) / 2 - image.offsetLeft;
-		        var moveY = (window.innerHeight - image.offsetHeight * 1.2) / 2 - image.offsetTop;
-		        image.style.transform += ' translate(' + moveX + 'px, ' + moveY + 'px)';
-		        image.style.zIndex = 999; 
-		    });
-		    images[i].addEventListener('mouseout', function(e) {
-		        e.target.style.transform = 'none';
-		        e.target.style.zIndex = 1; 
-		    });
-    
-		}
+for (var i = 0; i < images.length; i++) {
+  var parentDiv = images[i].parentNode;
+  var x = Math.floor(Math.random() * (parentDiv.offsetWidth - images[i].offsetWidth));
+  var y = Math.floor(Math.random() * (parentDiv.offsetHeight - images[i].offsetHeight));
+
+  images[i].style.top = y  + 50 + 'px';
+  images[i].style.left = x + 'px';
+}
